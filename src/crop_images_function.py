8ec9bf7eb5a0cabc3449c63802f29fe8e5e36e13
Andrea Sánchez <a348644@uabc.edu.mx>
2020-10-03 03:52:20 -0700

    Divide the script in functions

--- src/crop_images_function.py
+++ src/crop_images_function.py
@@ -1,24 +1,32 @@
 from PIL import Image
 import os
 
-archivos_tu_ciencia = os.listdir("Tu_Ciencia")
-carpetas = [elemento for elemento in archivos_tu_ciencia if os.path.isdir(elemento) and not elemento.startswith(".")]
-fotos = []
-basurita = [".ipynb", ".pdf", ".pptx", ".ppt", ".py", ".md", ".git"]
 
-for base_path, dirs, files in os.walk("."):
-    #base_path = os.path.join(path[0], *path[1:][0])
-    for file in files:
-        if not file.startswith(".") and not os.path.splitext(file)[1] in basurita:
-            fotos.append(os.path.join(base_path, file))
+def search_pictures(carpeta): 
+    archivos_tu_ciencia = os.listdir(carpeta)
+    carpetas = [elemento for elemento in archivos_tu_ciencia if os.path.isdir(elemento) and not elemento.startswith(".")]
+    fotos = []
+    basurita = [".ipynb", ".pdf", ".pptx", ".ppt", ".py", ".md", ".git"]
 
-for file in fotos:
-    img = Image.open(file)
-    ancho, alto = img.size
-    nuevo_alto = alto * 4 / 6
-    origen_y = (alto - nuevo_alto) / 2
-    extremo_y = nuevo_alto + origen_y
-    box = (0, origen_y, ancho, extremo_y)
-    area = img.crop(box)
-    #name = os.path.splitext(img)
-    area.save(file, 'jpeg')
+    for base_path, dirs, files in os.walk("."):
+        for file in files:
+         if not file.startswith(".") and not os.path.splitext(file)[1] in basurita:
+                fotos.append(os.path.join(base_path, file))
+    return (fotos)
+
+def crop_pictures(fotos):
+    for file in fotos:
+        img = Image.open(file)
+        ancho, alto = img.size
+        nuevo_alto = alto * 4 / 6
+        origen_y = (alto - nuevo_alto) / 2
+        extremo_y = nuevo_alto + origen_y
+        box = (0, origen_y, ancho, extremo_y)
+        area = img.crop(box)
+        area.save(file, 'jpeg')
+
+#MAIN
+
+photos_1 = search_pictures("Tu_Ciencia")
+crop_pictures(photos_1)
+
